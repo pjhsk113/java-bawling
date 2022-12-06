7e9be6b0cb01ba17a361273b9742dfd027ef8e3c
JanghoPark <pjhsk113@naver.com>
2022-12-06 23:04:05 +0900

    feat: answer 일급 컬렉션 추가

---
+++ src/main/java/qna/domain/Answers.java
@@ -0,0 +1,31 @@
+package qna.domain;
+
+import org.hibernate.annotations.Where;
+import qna.CannotDeleteException;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import java.util.ArrayList;
+import java.util.List;
+
+@Embeddable
+public class Answers {
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
+    @Where(clause = "deleted = false")
+    @OrderBy("id ASC")
+    private List<Answer> answers = new ArrayList<>();
+
+    public void validateDelete (User loginUser) throws CannotDeleteException {
+        boolean isMatch = answers.stream()
+                .anyMatch(answer -> !answer.isOwner(loginUser));
+        if (isMatch) {
+            throw new CannotDeleteException("다른 사람이 쓴 답변이 있어 삭제할 수 없습니다.");
+        }
+    }
+
+    public void add (Answer answer) {
+        answers.add(answer);
+    }
+}
