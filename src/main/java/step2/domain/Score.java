0fe0ad0e6c01f3c514cb696485ed36cc654bd132
JanghoPark <pjhsk113@naver.com>
2022-12-10 16:56:58 +0900

    feat: 볼링 점수 담당 객체 생성

---
+++ src/main/java/step2/domain/Score.java
@@ -0,0 +1,27 @@
+package step2.domain;
+
+public class Score {
+    private static final int MAX_SCORE = 10;
+    private static final int MIN_SCORE = 0;
+
+    private final int score;
+
+    private Score(int score) {
+        this.score = score;
+    }
+
+    public static Score from(int score) {
+        validate(score);
+        return new Score(score);
+    }
+
+    private static void validate(int score) {
+        if (score > MAX_SCORE || score < MIN_SCORE) {
+            throw new IllegalArgumentException("점수는 0 ~ 10 사이 값이어야 합니다.");
+        }
+    }
+
+    public boolean isStrike() {
+        return score == MAX_SCORE;
+    }
+}
