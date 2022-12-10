8aca3a05b03a67499c0c8eb4e9cbb2c0f78965f6
JanghoPark <pjhsk113@naver.com>
2022-12-10 16:55:56 +0900

    feat: InputView 생성

---
+++ src/main/java/step2/view/InputView.java
@@ -0,0 +1,24 @@
+package step2.view;
+
+import step2.domain.Player;
+import step2.domain.Score;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static final String INPUT_PLAYER_MESSAGE = "플레이어 이름은(3 english letters)?";
+    private static final String INPUT_PITCHING_MESSAGE = "%d프레임 투구";
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    private InputView() { }
+
+    public static Player inputPlayerName() {
+        System.out.println(INPUT_PLAYER_MESSAGE);
+        return Player.from(SCANNER.nextLine());
+    }
+
+    public static Score inputFramePitching(int round) {
+        System.out.println(String.format(INPUT_PITCHING_MESSAGE, round));
+        return Score.from(SCANNER.nextInt());
+    }
+}
