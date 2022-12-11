e0ea3f213662790ae66b9c4bec84c3afaf7e9dd3
JanghoPark <pjhsk113@naver.com>
2022-12-11 20:31:32 +0900

    feat: OutputView 생성

---
+++ src/main/java/step2/view/OutputView.java
@@ -0,0 +1,14 @@
+package step2.view;
+
+import step2.domain.Player;
+import step2.domain.Score;
+
+import java.util.Scanner;
+
+public class OutputView {
+    private static final String ROUND_FORMAT = "| NAME |%s|";
+
+    private OutputView() {
+    }
+
+}
