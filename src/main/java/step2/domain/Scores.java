5ed75d8939bfb8408529b22f19a0d3ab87d55810
JanghoPark <pjhsk113@naver.com>
2022-12-11 20:32:56 +0900

    refactor: 프레임 별 투구 점수 생성 로직 변경
    - 스트라이크, 스페어 등

--- src/main/java/step2/domain/Scores.java
+++ src/main/java/step2/domain/Scores.java
@@ -1,7 +1,5 @@
 package step2.domain;
 
-import java.util.List;
-
 public class Scores {
     private Score firstScore;
     private Score secondScore;
@@ -12,6 +10,16 @@ public class Scores {
     }
 
     public static Scores of(Score firstScore, Score secondScore) {
+        if (firstScore.isStrike()) {
+            return new Scores(firstScore, null);
+        }
+        validate(firstScore, secondScore);
         return new Scores(firstScore, secondScore);
+    }
+
+    private static void validate(Score firstScore, Score secondScore) {
+        if (firstScore.sum(secondScore) > 10) {
+            throw new IllegalArgumentException("프레임 별 투구 점수는 10을 넘을 수 없습니다.");
+        }
     }
 }
