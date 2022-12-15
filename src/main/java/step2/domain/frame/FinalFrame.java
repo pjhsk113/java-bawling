5d730d8091f53feae37cddb1c2f5c559f0abb42b
JanghoPark <pjhsk113@naver.com>
2022-12-15 23:07:07 +0900

    feat: 마지막 프레임은 투구 조건이 추가되므로 FinalFrame 생성

---
+++ src/main/java/step2/domain/frame/FinalFrame.java
@@ -0,0 +1,14 @@
+package step2.domain.frame;
+
+import step2.domain.score.Scores;
+
+public class FinalFrame extends Frame {
+
+    private FinalFrame(int frame, Scores scores) {
+        super(frame, scores);
+    }
+
+    public static FinalFrame of(int frame, Scores scores) {
+        return new FinalFrame(frame, scores);
+    }
+}
