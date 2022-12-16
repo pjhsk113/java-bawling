3736be555a07503d53960de55bfd819242dc146c
JanghoPark <pjhsk113@naver.com>
2022-12-16 23:14:18 +0900

    feat: 일반 프레임의 추상 메서드 구현
    - 첫 번째, 두 번째 투구 여부에 따라 객체를 생성하는 기능 추가
    - 모든 투구가 끝났는지 판단하는 기능 추가

--- src/main/java/step2/domain/score/NormalScores.java
+++ src/main/java/step2/domain/score/NormalScores.java
@@ -1,7 +1,6 @@
 package step2.domain.score;
 
 import step2.domain.Score;
-import step2.domain.score.Scores;
 
 public class NormalScores extends Scores {
 
@@ -12,5 +11,29 @@ public class NormalScores extends Scores {
     public static NormalScores of(Score firstScore, Score secondScore) {
         validate(firstScore, secondScore);
         return new NormalScores(firstScore, secondScore);
+    }
+
+    public static NormalScores init() {
+        return new NormalScores(null, null);
+    }
+
+    private static void validate(Score firstScore, Score secondScore) {
+        if (firstScore.sum(secondScore) > 10) {
+            throw new IllegalArgumentException("프레임 별 투구 점수는 10을 넘을 수 없습니다.");
+        }
+    }
+
+    @Override
+    public Scores next(Score score) {
+        if (firstScore == null) {
+            return of(score, null);
+        }
+
+        return of(firstScore, score);
+    }
+
+    @Override
+    public boolean isFrameOver() {
+        return firstScore != null && secondScore != null;
     }
 }
