148971870c197439fa2db082dd7a48768312b01c
JanghoPark <pjhsk113@naver.com>
2022-12-16 23:16:39 +0900

    feat: 일반 프레임의 생성 라이프 사이클 구현

--- src/main/java/step2/domain/frame/NormalFrame.java
+++ src/main/java/step2/domain/frame/NormalFrame.java
@@ -1,5 +1,7 @@
 package step2.domain.frame;
 
+import step2.domain.score.FinalScores;
+import step2.domain.score.NormalScores;
 import step2.domain.score.Scores;
 
 public class NormalFrame extends Frame {
@@ -10,5 +12,32 @@ public class NormalFrame extends Frame {
 
     public static NormalFrame of(int frame, Scores scores) {
         return new NormalFrame(frame, scores);
+    }
+
+    public static NormalFrame init() {
+        return NormalFrame.of(1, NormalScores.init());
+    }
+
+    @Override
+    public Frame next(Scores scores) {
+        if (isFrameNotOver()) {
+            return of(frame, scores);
+        }
+
+        int nextFrame = frame + 1;
+
+        if (isNormalFrame(nextFrame)) {
+            return of(nextFrame, NormalScores.init());
+        }
+
+        return FinalFrame.of(nextFrame, FinalScores.init());
+    }
+
+    private boolean isFrameNotOver() {
+        return !scores.isStrike() && !scores.isFrameOver();
+    }
+
+    private boolean isNormalFrame(int frame) {
+        return frame < 10;
     }
 }
