2db784a4916ed5f10060a63dd51a3fceb6921438
JanghoPark <pjhsk113@naver.com>
2022-12-16 23:21:45 +0900

    feat: 볼링 게임 라이프 사이클 구현
    - 일반 프레임 투구와 생성

--- src/main/java/step2/domain/BawlingGameExecutor.java
+++ src/main/java/step2/domain/BawlingGameExecutor.java
@@ -1,13 +1,40 @@
 package step2.domain;
 
+import step2.domain.frame.Frame;
+import step2.domain.frame.NormalFrame;
+import step2.domain.score.NormalScores;
+import step2.domain.score.Scores;
 import step2.view.InputView;
+import step2.view.OutputView;
 
 public class BawlingGameExecutor {
     public void play() {
         Player player = InputView.inputPlayerName();
+        OutputView.printFrame(player);
+
+        Frame frame = NormalFrame.init();
+        while(isNormalFrame(frame)) {
+            frame = normalFrame(frame);
+        }
+
     }
 
-    private void frameView() {
+    private boolean isNormalFrame(Frame frame) {
+        return frame instanceof NormalFrame;
+    }
 
+    private Frame normalFrame(Frame frame) {
+        Frame nextFrame = frameView(frame);
+        if (nextFrame.getScores().isStrike()) {
+            nextFrame = frameView(nextFrame);
+        }
+        return nextFrame;
+    }
+
+    private Frame frameView(Frame frame) {
+        Score score = InputView.inputFrameScore(frame.getFrame());
+        Scores scores = frame.getScores().next(score);
+        Frame nextFrame = frame.next(scores);
+        return nextFrame;
     }
 }
