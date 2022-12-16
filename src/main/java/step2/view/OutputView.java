e7f2964b7a01fba551a76ec02e93c604d70647a5
JanghoPark <pjhsk113@naver.com>
2022-12-16 23:18:55 +0900

    feat: 프레임 템플릿 출력 기능 추가(플레이어 점수판 그리기는 아직 미구현)

--- src/main/java/step2/view/OutputView.java
+++ src/main/java/step2/view/OutputView.java
@@ -4,6 +4,9 @@ import step2.domain.Player;
 import step2.domain.Score;
 
 import java.util.Scanner;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.joining;
 
 public class OutputView {
     private static final String ROUND_FORMAT = "| NAME |%s|";
@@ -11,4 +14,16 @@ public class OutputView {
     private OutputView() {
     }
 
+    public static void printFrame(Player player) {
+        System.out.println(printFramesTemplate());
+    }
+
+    private static String printFramesTemplate() {
+        return String.format(
+                ROUND_FORMAT,
+                IntStream.rangeClosed(1, 10)
+                        .mapToObj(frameRound -> String.format("  %02d  ", frameRound))
+                        .collect(joining("|"))
+        );
+    }
 }
