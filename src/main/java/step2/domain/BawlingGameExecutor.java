76e01f4ce3c4d3278b743b86754f30b99a1a1d4f
JanghoPark <pjhsk113@naver.com>
2022-12-17 15:13:40 +0900

    feat: 비즈니스 로직 작성
    - 제대로 동작 안함 ㅠㅠ 개선 필요

--- src/main/java/step2/domain/BawlingGameExecutor.java
+++ src/main/java/step2/domain/BawlingGameExecutor.java
@@ -1,8 +1,8 @@
 package step2.domain;
 
 import step2.domain.frame.Frame;
+import step2.domain.frame.Frames;
 import step2.domain.frame.NormalFrame;
-import step2.domain.score.NormalScores;
 import step2.domain.score.Scores;
 import step2.view.InputView;
 import step2.view.OutputView;
@@ -10,31 +10,46 @@ import step2.view.OutputView;
 public class BawlingGameExecutor {
     public void play() {
         Player player = InputView.inputPlayerName();
-        OutputView.printFrame(player);
+        Frames frames = Frames.init();
+        OutputView.printFrame(player, frames);
 
         Frame frame = NormalFrame.init();
         while(isNormalFrame(frame)) {
-            frame = normalFrame(frame);
+            frame = normalFrame(frame, player, frames);
         }
-
+        finalFrame(frame, player, frames);
     }
 
     private boolean isNormalFrame(Frame frame) {
         return frame instanceof NormalFrame;
     }
 
-    private Frame normalFrame(Frame frame) {
-        Frame nextFrame = frameView(frame);
+    private Frame normalFrame(Frame frame, Player player, Frames frames) {
+        Frame nextFrame = frameView(frame, player, frames);
         if (nextFrame.getScores().isStrike()) {
-            nextFrame = frameView(nextFrame);
+            nextFrame = frameView(nextFrame, player, frames);
         }
         return nextFrame;
     }
 
-    private Frame frameView(Frame frame) {
+    private void finalFrame(Frame frame, Player player, Frames frames) {
+        Frame nextFrame = frameView(frame, player, frames);
+        nextFrame = frameView(nextFrame, player, frames);
+
+        if (isExtraFrame(nextFrame)) {
+            frameView(nextFrame, player, frames);
+        }
+    }
+
+    private boolean isExtraFrame(Frame frame) {
+        return frame.getScores().isStrike() || frame.getScores().isSpared();
+    }
+
+    private Frame frameView(Frame frame, Player player, Frames frames) {
         Score score = InputView.inputFrameScore(frame.getFrame());
         Scores scores = frame.getScores().next(score);
         Frame nextFrame = frame.next(scores);
+        OutputView.printFrame(player, frames.addFrames(nextFrame));
         return nextFrame;
     }
 }
--- src/main/java/step2/view/OutputView.java
+++ src/main/java/step2/view/OutputView.java
@@ -2,20 +2,29 @@ package step2.view;
 
 import step2.domain.Player;
 import step2.domain.Score;
+import step2.domain.ScoreType;
+import step2.domain.frame.Frame;
+import step2.domain.frame.Frames;
+import step2.domain.score.NormalScores;
 
-import java.util.Scanner;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.*;
 
 public class OutputView {
     private static final String ROUND_FORMAT = "| NAME |%s|";
+    private static final String SCORES_FORMAT = "| %4s |%s|";
 
     private OutputView() {
     }
 
-    public static void printFrame(Player player) {
+    public static void printFrame(Player player, Frames frames) {
         System.out.println(printFramesTemplate());
+        System.out.println(printScores(player, frames));
     }
 
     private static String printFramesTemplate() {
@@ -25,5 +34,47 @@ public class OutputView {
                         .mapToObj(frameRound -> String.format("  %02d  ", frameRound))
                         .collect(joining("|"))
         );
+    }
+
+    private static String printScores(Player player, Frames frames) {
+        return String.format(
+                SCORES_FORMAT,
+                player.getName(),
+                frames.stream()
+                        .map(frame -> String.format("%-4s", scoreOf(frame)))
+                        .collect(joining("|  "))
+        );
+    }
+
+    private static String scoreOf(Frame frame) {
+        if (frame == null) {
+            return "";
+        }
+
+        return eachScoreOf(
+                frame.getScores()
+                        .stream()
+                        .collect(toList()));
+    }
+    private static String eachScoreOf(List<Score> scores) {
+        return IntStream.range(0, scores.size())
+                .mapToObj(index -> scores.get(index) != null
+                        ? toScoreType(scores, index)
+                        : null)
+                .filter(Objects::nonNull)
+                .collect(joining("|"));
+    }
+
+    private static String toScoreType(List<Score> scores, int index) {
+        if (scores.get(index).isGutter()) {
+            return ScoreType.GUTTER.getSymbol();
+        }
+        if (scores.get(index).isStrike()) {
+            return ScoreType.STRIKE.getSymbol();
+        }
+        if (index == 1 && scores.get(0).isSpare(scores.get(1))) {
+            return ScoreType.SPARED.getSymbol();
+        }
+        return scores.get(index).toString();
     }
 }
