fa3aad90eba329a053550c781be2172c70253304
JanghoPark <pjhsk113@naver.com>
2022-12-18 16:30:10 +0900

    test: 패키지 경로 수정

--- src/test/java/step1/qna/service/QnaServiceTest.java
+++ src/test/java/step1/qna/service/QnaServiceTest.java
@@ -7,7 +7,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import step1.qna.CannotDeleteException;
-import qna.domain.*;
 import step1.qna.domain.*;
 
 import java.time.LocalDateTime;
