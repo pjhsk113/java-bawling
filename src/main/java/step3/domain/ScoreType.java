b3f62db7f0698f18d6cb8ad03e547b63a56fec25
JanghoPark <pjhsk113@naver.com>
2022-12-23 18:22:30 +0900

    refactor: 점수 타입 반환을 ScoreType이 책임 지도록 변경

--- src/main/java/step3/domain/ScoreType.java
+++ src/main/java/step3/domain/ScoreType.java
@@ -1,5 +1,7 @@
 package step3.domain;
 
+import java.util.List;
+
 public enum ScoreType {
     SPARED("/"),
     STRIKE("X"),
@@ -13,5 +15,21 @@ public enum ScoreType {
 
     public String getSymbol() {
         return symbol;
+    }
+
+    public static String convertSymbol(List<Score> scores, int index) {
+        if (scores.get(index).isGutter()) {
+            return ScoreType.GUTTER.getSymbol();
+        }
+
+        if (index == 1 && scores.get(0).isSpare(scores.get(1))) {
+            return ScoreType.SPARED.getSymbol();
+        }
+
+        if (scores.get(index).isStrike()) {
+            return ScoreType.STRIKE.getSymbol();
+        }
+
+        return scores.get(index).toString();
     }
 }
