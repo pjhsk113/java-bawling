3c8b42634b8b9ac54e832afa5765c2ff9cbdfc8d
JanghoPark <pjhsk113@naver.com>
2023-01-17 22:28:24 +0900

    feat: 여러 player를 담는 일급컬랙션

---
+++ src/main/java/step4/domain/Players.java
@@ -0,0 +1,20 @@
+package step4.domain;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Players {
+    private final List<Player> players;
+
+    private Players(List<Player> players) {
+        this.players = players;
+    }
+
+    public static Players of(List<Player> players) {
+        return new Players(players);
+    }
+
+    public Stream<Player> stream() {
+        return players.stream();
+    }
+}
